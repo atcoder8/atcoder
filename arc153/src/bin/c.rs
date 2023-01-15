d8d7d062669712538f4bde725758ee073e052445
atcoder8 <atcoder8@gmail.com>
2023-01-15 14:09:41 +0900

    Solved ARC153-C.

--- arc153/src/bin/c.rs
+++ arc153/src/bin/c.rs
@@ -1,92 +1,50 @@
-// unfinished
-
+use itertools::join;
+use itertools_num::ItertoolsNum;
 use proconio::input;
 
 fn main() {
-    println!("{}", if solve() { "Yes" } else { "No" });
+    if let Some(xx) = solve() {
+        println!("Yes");
+        println!("{}", join(xx, " "));
+    } else {
+        println!("No");
+    }
 }
 
-fn solve() -> bool {
+fn solve() -> Option<Vec<i64>> {
     input! {
         n: usize,
         aa: [i64; n],
     }
 
-    if n == 1 {
-        return true;
-    }
+    let mut bb: Vec<i64> = aa.iter().rev().cumsum().collect();
+    bb.reverse();
+    let sum_bb: i64 = bb.iter().sum();
 
-    let positive_num = aa.iter().filter(|&&a| a == 1).count();
-    let negative_num = n - positive_num;
+    if bb[0] != 0 {
+        let mut xx = vec![if bb[0] < 0 { 1 } else { -1 } * (sum_bb - bb[0])];
+        let abs_b0 = bb[0].abs();
 
-    // 0を含む
-    let left_pos_num = 0;
-    let left_neg_num = 0;
-
-    let right_pos_num = positive_num - (aa[0] == 1) as usize;
-    let right_neg_num = negative_num - (aa[0] == -1) as usize;
-
-    for zero_idx in 0..n {
-        let left_pos_edge_win = aa[0] == -1;
-        let left_pos_num_win = left_pos_num < left_neg_num;
-
-        let left_neg_edge_win = aa[0] == 1;
-        let left_neg_num_win = left_pos_num > left_neg_num;
-
-        let left_pos_win = aa[0] == -1 || left_pos_num < left_neg_num;
-        let left_neg_win = aa[0] == 1 || left_pos_num > left_neg_num;
-
-        let right_pos_win = aa[n - 1] == 1 || right_pos_num > right_neg_num;
-        let right_neg_win = aa[n - 1] == -1 || right_pos_num < right_neg_num;
-
-        if left_pos_win && right_neg_win {
-            
+        for i in 1..n {
+            xx.push(xx[i - 1] + abs_b0);
         }
+
+        return Some(xx);
     }
 
-    if aa[n - 1] == 1
-        && (positive_num < negative_num || (aa[0] == 1 && positive_num == negative_num))
-    {
-        return true;
+    if bb.iter().all(|&b| b <= 0) || bb.iter().all(|&b| b >= 0) {
+        return None;
     }
 
-    if aa[n - 1] == -1
-        && (positive_num > negative_num || (aa[0] == -1 && positive_num == negative_num))
-    {
-        return true;
+    let mut yy = vec![1; n];
+
+    if sum_bb >= 0 {
+        let p = bb.iter().position(|&b| b == -1).unwrap();
+        yy[p] = 1 + sum_bb;
+    } else {
+        let q = bb.iter().position(|&b| b == 1).unwrap();
+        yy[q] = 1 - sum_bb;
     }
 
-    if aa[0] == 1 && positive_num > negative_num || aa[0] == -1 && positive_num < negative_num {
-        return true;
-    }
-
-    let mut left_negative_num = 0;
-    let mut left_positive_num = 0;
-
-    let mut right_negative_num = n - positive_num;
-    let mut right_positive_num = positive_num;
-
-    for i in 0..(n - 1) {
-        left_negative_num += (aa[i] == -1) as usize;
-        left_positive_num += (aa[i] == 1) as usize;
-
-        right_negative_num -= (aa[i] == -1) as usize;
-        right_positive_num -= (aa[i] == 1) as usize;
-
-        let left_pos_win = aa[0] == -1 || left_positive_num < left_negative_num;
-        let left_neg_win = aa[0] == 1 || left_positive_num > left_negative_num;
-
-        let right_pos_win = aa[n - 1] == 1
-            || right_positive_num > right_negative_num
-            || (right_positive_num == right_negative_num && aa[i + 1] == -1);
-        let right_neg_win = aa[n - 1] == -1
-            || (right_positive_num < right_negative_num)
-            || (right_positive_num == right_negative_num && aa[i + 1] == 1);
-
-        if left_pos_win && right_neg_win || left_neg_win && right_pos_win {
-            return true;
-        }
-    }
-
-    false
+    Some(yy.iter().cumsum().collect())
 }
