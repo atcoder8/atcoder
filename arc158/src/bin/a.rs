c88556d6c87adda1921ad2b33a5188a98c8ad6f1
atcoder8 <atcoder8@gmail.com>
2023-03-13 05:34:12 +0900

    Solved ARC158-A again.

--- arc158/src/bin/a.rs
+++ arc158/src/bin/a.rs
@@ -7,11 +7,7 @@ fn main() {
     }
 
     for _ in 0..t {
-        input! {
-            xx: [usize; 3],
-        }
-
-        if let Some(ans) = solve(xx) {
+        if let Some(ans) = solve() {
             println!("{}", ans);
         } else {
             println!("-1");
@@ -19,19 +15,24 @@ fn main() {
     }
 }
 
-fn solve(xx: Vec<usize>) -> Option<usize> {
+fn solve() -> Option<usize> {
+    input! {
+        xx: [usize; 3],
+    }
+
     let sum_x: usize = xx.iter().sum();
 
     if !xx.iter().map(|&x| x % 2).all_equal() || sum_x % 3 != 0 {
         return None;
     }
 
-    let a = sum_x / 3;
-
-    Some(xx.iter().map(|&x| diff_abs(x, a)).sum::<usize>() / 4)
+    Some(xx.iter().map(|&x| abs_diff(x, sum_x / 3)).sum::<usize>() / 4)
 }
 
-fn diff_abs(a: usize, b: usize) -> usize {
+fn abs_diff<T>(a: T, b: T) -> T
+where
+    T: PartialOrd<T> + std::ops::Sub<T, Output = T>,
+{
     if a >= b {
         a - b
     } else {
