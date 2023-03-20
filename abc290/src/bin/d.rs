45840db5e364c7277a2e47aa4e31207b0e02d63a
atcoder8 <atcoder8@gmail.com>
2023-03-21 00:48:37 +0900

    Solved ABC290-D again.

--- abc290/src/bin/d.rs
+++ abc290/src/bin/d.rs
@@ -4,22 +4,14 @@ use proconio::input;
 fn main() {
     input! {
         t: usize,
+        ndk: [(usize, usize, usize); t],
     }
 
-    for _ in 0..t {
-        solve();
+    for &(n, d, k) in &ndk {
+        let cycle_len = n / n.gcd(&d);
+        let shift_num = (k - 1) / cycle_len;
+
+        let ans = ((k - 1) * d + shift_num) % n;
+        println!("{}", ans);
     }
-}
-
-fn solve() {
-    input! {
-        (n, d, k): (usize, usize, usize),
-    }
-
-    let period = n / n.gcd(&d);
-    let cycle_num = (k - 1) / period;
-    let rem = (k - 1) % period;
-
-    let ans = (d * rem + cycle_num) % n;
-    println!("{}", ans);
 }
