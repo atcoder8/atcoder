6aa691ba729185d98e62af0e45a76de3c68af716
atcoder8 <atcoder8@gmail.com>
2023-03-20 21:09:58 +0900

    Solved ABC294-D again.

--- abc294/src/bin/d.rs
+++ abc294/src/bin/d.rs
@@ -1,5 +1,3 @@
-use std::{cmp::Reverse, collections::BinaryHeap};
-
 use proconio::{input, marker::Usize1};
 
 fn main() {
@@ -7,33 +5,30 @@ fn main() {
         (n, q): (usize, usize),
     }
 
-    let mut heap = BinaryHeap::new();
     let mut used = vec![false; n];
-    let mut call_cnt = 0;
+    let mut min_wait_idx = 0;
 
     for _ in 0..q {
         input! {
             event_type: usize,
         }
 
-        if event_type == 1 {
-            heap.push(Reverse(call_cnt));
-            call_cnt += 1;
-        } else if event_type == 2 {
-            input! {
-                x: Usize1,
-            }
-
-            used[x] = true;
-        } else {
-            let ans = loop {
-                let Reverse(ans) = heap.pop().unwrap();
-                if !used[ans] {
-                    break ans;
+        match event_type {
+            2 => {
+                input! {
+                    x: Usize1,
                 }
-            };
-            heap.push(Reverse(ans));
-            println!("{}", ans + 1);
+
+                used[x] = true;
+            }
+            3 => {
+                while used[min_wait_idx] {
+                    min_wait_idx += 1;
+                }
+
+                println!("{}", min_wait_idx + 1);
+            }
+            _ => {}
         }
     }
 }
