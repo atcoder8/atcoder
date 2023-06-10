cd580e50b3361c825b36f11cfc61221cd9f7684d
atcoder8 <atcoder8@gmail.com>
2023-06-10 15:40:40 +0900

    Solved problem ABC304-B again.

--- abc304/src/bin/b.rs
+++ abc304/src/bin/b.rs
@@ -2,22 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        mut n: usize,
     }
 
-    if n < 1000 {
-        println!("{}", n);
-    } else if n < 10_000 {
-        println!("{}", n / 10 * 10);
-    } else if n < 100_000 {
-        println!("{}", n / 100 * 100);
-    } else if n < 1_000_000 {
-        println!("{}", n / 1000 * 1000);
-    } else if n < 10_000_000 {
-        println!("{}", n / 10000 * 10000);
-    } else if n < 100_000_000 {
-        println!("{}", n / 100_000 * 100_000);
-    } else {
-        println!("{}", n / 1_000_000 * 1_000_000);
+    let mut ten = 1;
+    while n >= 1000 {
+        n /= 10;
+        ten *= 10;
     }
+    n *= ten;
+
+    println!("{}", n);
 }
