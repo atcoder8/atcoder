2266aa879f79430e3c2124caf1a941321cc51b4f
atcoder8 <atcoder8@gmail.com>
2023-06-18 11:51:08 +0900

    Re-solved ABC306-E using `multiset` library.

--- abc306/src/bin/e.rs
+++ abc306/src/bin/e.rs
@@ -1,6 +1,6 @@
-use std::collections::BTreeMap;
-
 use proconio::{input, marker::Usize1};
+
+use crate::multiset::Multiset;
 
 fn main() {
     input! {
@@ -10,51 +10,169 @@ fn main() {
 
     let mut fa = 0;
     let mut aa = vec![0_usize; n];
-    let mut bb: BTreeMap<usize, usize> = BTreeMap::new();
-    let mut rem = BTreeMap::new();
-    bb.insert(0, k);
-    rem.insert(0, n - k);
+    let mut large: Multiset<usize> = vec![0; k].into();
+    let mut small: Multiset<usize> = vec![0; n - k].into();
 
     for &(x, y) in &xy {
         let elem = &mut aa[x];
 
-        if let Some(cnt) = rem.get_mut(elem) {
-            *cnt -= 1;
-            if *cnt == 0 {
-                rem.remove(elem);
-            }
-        } else {
-            if let Some(cnt) = bb.get_mut(elem) {
-                fa -= *elem;
-                *cnt -= 1;
-                if *cnt == 0 {
-                    bb.remove(elem);
-                }
-            }
+        if small.remove(elem).is_none() {
+            large.remove(elem);
+            fa -= *elem;
 
-            if let Some((&max_key, max_value)) = rem.iter_mut().next_back() {
-                fa += max_key;
-                *bb.entry(max_key).or_insert(0) += 1;
-                *max_value -= 1;
-                if *max_value == 0 {
-                    rem.remove(&max_key);
-                }
+            if let Some(&max) = small.max_element() {
+                small.remove(&max);
+                large.insert(max);
+                fa += max;
             }
         }
 
         *elem = y;
 
-        *bb.entry(y).or_insert(0) += 1;
-        fa += y;
+        large.insert(*elem);
+        fa += *elem;
 
-        let (&min_key, min_value) = bb.iter_mut().next().unwrap();
-        fa -= min_key;
-        *min_value -= 1;
-        if *min_value == 0 {
-            bb.remove(&min_key);
+        if large.len() > k {
+            let min = *large.min_element().unwrap();
+            large.remove(&min);
+            small.insert(min);
+            fa -= min;
         }
-        *rem.entry(min_key).or_insert(0) += 1;
 
         println!("{}", fa);
+    }
+}
+
+pub mod multiset {
+    use std::{collections::BTreeMap, iter::FromIterator};
+
+    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    pub struct Multiset<T>
+    where
+        T: Ord,
+    {
+        map: BTreeMap<T, usize>,
+        len: usize,
+    }
+
+    impl<T> Default for Multiset<T>
+    where
+        T: Ord,
+    {
+        fn default() -> Self {
+            Self {
+                map: Default::default(),
+                len: Default::default(),
+            }
+        }
+    }
+
+    impl<T> FromIterator<T> for Multiset<T>
+    where
+        T: Ord,
+    {
+        fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+            let mut ms = Multiset::new();
+            for value in iter {
+                ms.insert(value);
+            }
+
+            ms
+        }
+    }
+
+    impl<T> From<Vec<T>> for Multiset<T>
+    where
+        T: Ord,
+    {
+        fn from(value: Vec<T>) -> Self {
+            value.into_iter().collect()
+        }
+    }
+
+    impl<T> Multiset<T>
+    where
+        T: Ord,
+    {
+        pub fn new() -> Self {
+            Self {
+                map: BTreeMap::new(),
+                len: 0,
+            }
+        }
+
+        pub fn is_empty(&self) -> bool {
+            self.len == 0
+        }
+
+        pub fn len(&self) -> usize {
+            self.len
+        }
+
+        /// Adds a `value` to the set.
+        ///
+        /// Returns the number of elements equal to `value` in the multiset.
+        pub fn insert(&mut self, value: T) -> usize {
+            let count = self.map.entry(value).or_default();
+            *count += 1;
+            self.len += 1;
+
+            *count
+        }
+
+        /// If the multiset contains an element equal to `value`, removes it from the multiset.
+        ///
+        /// Returns the number of elements equal to `value` in the multiset after deletion.
+        /// However, if the multiset does not contain any `value`, `None` is returned.
+        pub fn remove(&mut self, value: &T) -> Option<usize> {
+            if let Some(count) = self.map.get_mut(value) {
+                *count -= 1;
+                self.len -= 1;
+
+                if *count == 0 {
+                    self.map.remove(value);
+
+                    return Some(0);
+                }
+
+                return Some(*count);
+            }
+
+            return None;
+        }
+
+        pub fn contains(&self, value: &T) -> bool {
+            self.map.contains_key(value)
+        }
+
+        pub fn count(&self, value: &T) -> usize {
+            *self.map.get(value).unwrap_or(&0)
+        }
+
+        pub fn min_element(&self) -> Option<&T> {
+            if let Some((value, _)) = self.map.iter().next() {
+                Some(value)
+            } else {
+                None
+            }
+        }
+
+        pub fn max_element(&self) -> Option<&T> {
+            if let Some((value, _)) = self.map.iter().next_back() {
+                Some(value)
+            } else {
+                None
+            }
+        }
+
+        pub fn clear(&mut self) {
+            self.map.clear();
+            self.len = 0;
+        }
+
+        pub fn remove_all(&mut self, value: &T) {
+            self.len -= self.count(value);
+            self.map.remove(&value);
+        }
     }
 }
