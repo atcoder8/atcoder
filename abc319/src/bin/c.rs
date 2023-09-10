b1a3979780a90f813eb8c76c64b118b69d6d51e4
atcoder8 <atcoder8@gmail.com>
2023-09-10 17:18:18 +0900

    Solved ABC319-C again.

--- abc319/src/bin/c.rs
+++ abc319/src/bin/c.rs
@@ -1,67 +1,41 @@
 use itertools::Itertools;
 use proconio::input;
 
+const LINES: [[usize; 3]; 8] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
 fn main() {
     input! {
-        ccc: [[usize; 3]; 3],
+        cc: [usize; 9],
     }
 
-    let check = |known: &Vec<Vec<bool>>| {
-        for i in 0..3 {
-            let seq = (0..3)
-                .filter(|&j| known[i][j])
-                .map(|j| ccc[i][j])
-                .collect_vec();
-            if seq.len() == 2 && seq.iter().all_equal() {
+    let is_ok = |orders: &[usize]| {
+        for line in LINES {
+            if orders
+                .iter()
+                .filter(|&&order| line.contains(&order))
+                .map(|&order| cc[order])
+                .take(2)
+                .all_equal()
+            {
                 return false;
             }
-        }
-
-        for j in 0..3 {
-            let seq = (0..3)
-                .filter(|&i| known[i][j])
-                .map(|i| ccc[i][j])
-                .collect_vec();
-            if seq.len() == 2 && seq.iter().all_equal() {
-                return false;
-            }
-        }
-
-        let seq = (0..3)
-            .filter(|&i| known[i][i])
-            .map(|i| ccc[i][i])
-            .collect_vec();
-        if seq.len() == 2 && seq.iter().all_equal() {
-            return false;
-        }
-
-        let seq = (0..3)
-            .filter(|&i| known[i][2 - i])
-            .map(|i| ccc[i][2 - i])
-            .collect_vec();
-        if seq.len() == 2 && seq.iter().all_equal() {
-            return false;
         }
 
         true
     };
 
-    let mut cnt = 0;
-    for orders in (0..9).permutations(9) {
-        let mut flag = true;
-        let mut known = vec![vec![false; 3]; 3];
-        for i in 0..9 {
-            let order = orders[i];
-            known[order / 3][order % 3] = true;
-
-            if !check(&known) {
-                flag = false;
-                break;
-            }
-        }
-
-        cnt += flag as usize;
-    }
-
-    println!("{}", cnt as f64 / 362880.0);
+    let cnt_ok = (0..9)
+        .permutations(9)
+        .filter(|orders| is_ok(orders))
+        .count();
+    println!("{}", cnt_ok as f64 / 362880.0);
 }
