3e52f114920576d5d5a25adc40224010fe2a5fbf
atcoder8 <atcoder8@gmail.com>
2023-09-24 00:30:04 +0900

    Solved ABC321-E.

--- abc321/src/bin/e.rs
+++ abc321/src/bin/e.rs
@@ -1,10 +1,9 @@
-// unfinished
-
 use proconio::input;
 
 fn main() {
     input! {
-        nxk: [(usize, usize, usize)]
+        t: usize,
+        nxk: [(usize, usize, usize); t],
     }
 
     for &(n, x, k) in &nxk {
@@ -13,51 +12,25 @@ fn main() {
 }
 
 fn solve(n: usize, x: usize, k: usize) -> usize {
-    if k == 0 {
-        return 1;
-    }
-
-    let calc_rank = |node: usize| (1_usize..).find(|&i| node >> i == 0).unwrap() - 1;
-
-    let n_rank = calc_rank(n);
-    let x_rank = calc_rank(x);
-
-    let mut ans = 0;
-
-    // K代下の子孫
-    if k <= 60 && n >= x << k {
-        ans += (((x + 1) << k) - 1).min(n) - (x << k) + 1;
-    }
-
-    // i代上の祖先で折返し(1<=i<=K-2)
-    if k >= 2 {
-        if k <= x_rank + 2 {
-            ans += 2_usize.pow((k - 1) as u32) - 1;
+    let calc_descendants = |root: usize, dist: usize| {
+        if dist <= 60 && root <= n >> dist {
+            (((root + 1) << dist) - 1).min(n) - (root << dist) + 1
+        } else {
+            0
         }
-    }
-    let min = if x_rank + k < n_rank {
-        1
-    } else {
-        (x_rank + k - n_rank + 1) / 2 + 1
     };
-    let max = (k - 2).min(x_rank);
-    if min <= max {
-        ans += 2_usize.pow((k - min) as u32) - 1;
-        if max < k {
-            ans -= 2_usize.pow((k - max) as u32) - 1;
-        }
-        if k >= 2 * (min - 1) {
-            ans += ((x + 1) << (k - 2 * (min - 1))).min(n) - (x << (k - 2 * (min - 1))).min(n) + 1;
-        }
-    }
 
-    // K-1代上の祖先で折返し
-    if x_rank >= k - 1 {
-        ans += 1;
-    }
+    let mut ans = calc_descendants(x, k);
+    let mut cur = x;
+    for back in 1..=k.saturating_sub(1) {
+        if cur == 1 {
+            break;
+        }
 
-    // K代上の祖先で折返し
-    if x_rank >= k {
+        ans += calc_descendants(cur ^ 1, k - (back + 1));
+        cur /= 2;
+    }
+    if k <= 60 && k != 0 && x >> k != 0 {
         ans += 1;
     }
 
