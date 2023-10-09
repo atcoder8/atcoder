0afff48fe8ed0504adfc0cbcaf70c92efe906eb4
atcoder8 <atcoder8@gmail.com>
2023-10-09 15:07:05 +0900

    The code of ARC166-B has been further shortened.

--- arc166/b.py
+++ arc166/b.py
@@ -1,15 +1,11 @@
 import math
 a=lambda:list(map(int,input().split()))
-b,c=a()[1:],a()
-d=[10**18]*8
-d[0]=0
-for e in c:
-    f=list(d)
-    for g in range(7):
-        h=i=7^g
-        while i:
-            j=math.lcm(*(b[k]for k in(0,1,2)if(i>>k)&1))
-            f[g|i]=min(f[g|i],d[g]+(j-e%j)%j)
-            i=(i-1)&h
-    d=f
-print(d[7])
+b=a()[1:]
+c=[0]+[10**18]*7
+for d in a():
+ e=list(c)
+ for f in range(7):
+  g=h=7^f
+  while h:i=math.lcm(*(b[j]for j in(0,1,2)if h>>j&1));e[f|h]=min(e[f|h],c[f]+(i-d%i)%i);h=(h-1)&g
+ c=e
+print(c[7])
