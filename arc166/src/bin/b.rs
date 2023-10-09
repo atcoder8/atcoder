494970c5be2778a53c0b2a3615627fca4b719bd0
atcoder8 <atcoder8@gmail.com>
2023-10-09 10:34:36 +0900

    Solved ARC166-B.

--- arc166/src/bin/b.rs
+++ arc166/src/bin/b.rs
@@ -1,74 +1,33 @@
-// unfinished
-
-use std::collections::BTreeSet;
-
 use num_integer::Integer;
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        divisors: [usize; 3],
+        dd: [usize; 3],
         aa: [usize; n],
     }
 
-    let calc_op_num = |a: usize, divisor: usize| (a + divisor - 1) / divisor * divisor - a;
+    let mut dp = vec![10_usize.pow(18); 8];
+    dp[0] = 0;
+    for &a in &aa {
+        let mut next_dp = dp.clone();
 
-    let mut op_counts = vec![vec![0; n]; 8];
-    for (i, &a) in aa.iter().enumerate() {
-        for bit in 1..8 {
-            let lcm = (0..3)
-                .filter(|&j| (bit >> j) & 1 == 1)
-                .fold(1, |acc, j| acc.lcm(&divisors[j]));
-            op_counts[bit][i] = calc_op_num(a, lcm);
-        }
-    }
+        for bit in 0..7 {
+            let rem = 7 ^ bit;
+            let mut add = rem;
+            while add != 0 {
+                let d = (0..3)
+                    .filter(|&i| (add >> i) & 1 == 1)
+                    .fold(1, |acc, i| acc.lcm(&dd[i]));
+                next_dp[bit | add] = next_dp[bit | add].min(dp[bit] + (d - a % d) % d);
 
-    let mut op_counts_set = vec![BTreeSet::new(); 8];
-    for bit in 0..8 {
-        op_counts_set[bit] = op_counts[bit]
-            .iter()
-            .enumerate()
-            .map(|(i, &op_cnt)| (op_cnt, i))
-            .collect();
-    }
-
-    let ans = dfs(&op_counts, &mut op_counts_set, 0).unwrap();
-    println!("{}", ans);
-}
-
-fn dfs(
-    op_counts: &Vec<Vec<usize>>,
-    op_counts_set: &mut Vec<BTreeSet<(usize, usize)>>,
-    satisfied: usize,
-) -> Option<usize> {
-    if satisfied == 7 {
-        return Some(0);
-    }
-
-    for bit in 1..8 {
-        if bit & satisfied == 0 {
-            if let Some(&(op_cnt_1, idx)) = op_counts_set[bit].first() {
-                op_counts_set
-                    .iter_mut()
-                    .enumerate()
-                    .for_each(|(bit, op_cnt)| {
-                        op_cnt.remove(&(op_counts[bit][idx], idx));
-                    });
-
-                if let Some(op_cnt_2) = dfs(op_counts, op_counts_set, satisfied | bit) {
-                    return Some(op_cnt_1 + op_cnt_2);
-                }
-
-                op_counts_set
-                    .iter_mut()
-                    .enumerate()
-                    .for_each(|(bit, op_cnt)| {
-                        op_cnt.insert((op_counts[bit][idx], idx));
-                    });
+                add = (add - 1) & rem;
             }
         }
+
+        dp = next_dp;
     }
 
-    None
+    println!("{}", dp[7]);
 }
