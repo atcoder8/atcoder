f408c42fb9cdb431398f31cb74e1506f4cdc3ef0
atcoder8 <atcoder8@gmail.com>
2023-11-25 23:37:02 +0900

    Solved ABC330-C again.

--- abc330/src/bin/c.rs
+++ abc330/src/bin/c.rs
@@ -7,8 +7,8 @@ fn main() {
     }
 
     let mut ans = d;
-    for x in 0..10_usize.pow(6) {
-        let y = d.saturating_sub(x.pow(2)).sqrt();
+    for x in 0..=d.sqrt() {
+        let y = (d - x.pow(2)).sqrt();
         ans = ans.min(d.abs_diff(x.pow(2) + y.pow(2)));
         ans = ans.min(d.abs_diff(x.pow(2) + (y + 1).pow(2)));
     }
