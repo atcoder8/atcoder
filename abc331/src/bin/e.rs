8628dea08e01fd836c016e2a21ff6cd9bfa59f23
atcoder8 <atcoder8@gmail.com>
2023-12-03 14:25:20 +0900

    Solved ABC331-E.

--- abc331/src/bin/e.rs
+++ abc331/src/bin/e.rs
@@ -12,42 +12,24 @@ fn main() {
         cd: [(Usize1, Usize1); l],
     }
 
-    let ia = enumerate(aa.clone())
+    let ia = enumerate(aa)
         .sorted_unstable_by_key(|x| Reverse(x.1))
         .collect_vec();
-    let jb = enumerate(bb.clone())
+    let jb = enumerate(bb)
         .sorted_unstable_by_key(|x| Reverse(x.1))
         .collect_vec();
 
     let unsupported: HashSet<(usize, usize)> = cd.iter().cloned().collect();
 
-    let is_ok = |key: usize| {
-        for &(i, a) in &ia {
-            for &(j, b) in &jb {
-                if a + b < key {
-                    break;
-                }
-
-                if !unsupported.contains(&(i, j)) {
-                    return true;
-                }
+    let mut ans = 0;
+    for &(i, a) in &ia {
+        for &(j, b) in &jb {
+            if !unsupported.contains(&(i, j)) {
+                ans = ans.max(a + b);
+                break;
             }
-        }
-
-        false
-    };
-
-    let mut ok = 0;
-    let mut ng = 10_usize.pow(10);
-    while ng - ok > 1 {
-        let mid = (ok + ng) / 2;
-
-        if is_ok(mid) {
-            ok = mid;
-        } else {
-            ng = mid;
         }
     }
 
-    println!("{}", ok);
+    println!("{}", ans);
 }
