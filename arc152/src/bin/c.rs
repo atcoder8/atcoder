39e83697be94443ad057ca5ae670a1bafc3110bf
atcoder8 <atcoder8@gmail.com>
2023-12-08 00:30:06 +0900

    Solved ARC152.

--- arc152/src/bin/c.rs
+++ arc152/src/bin/c.rs
@@ -1,40 +1,13 @@
-// unfinished
-
-use itertools::Itertools;
-use num::Integer;
+use num_integer::Integer;
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        mut aa: [usize; n],
+        aa: [usize; n],
     }
 
-    let mut acc_gcd = 0;
-
-    for &a in &aa {
-        if aa[n - 1] > 2 * a {
-            continue;
-        }
-
-        acc_gcd = acc_gcd.gcd(&a);
-    }
-
-    let bb = aa.iter().rev().map(|&a| aa[n - 1] - a).collect_vec();
-
-    for b in &bb {
-        if bb[n - 1] > 2 * b {
-            continue;
-        }
-
-        acc_gcd = acc_gcd.gcd(&b);
-    }
-
-    let ans = if acc_gcd == 0 {
-        aa[n - 1]
-    } else {
-        aa[0] + acc_gcd
-    };
-
-    println!("{}", aa[n - 1] - acc_gcd);
+    let d = aa[n - 1] - aa[0];
+    let ans = aa[0] % aa.iter().fold(d, |acc, &a| acc.gcd(&(2 * (a - aa[0])))) + d;
+    println!("{}", ans);
 }
