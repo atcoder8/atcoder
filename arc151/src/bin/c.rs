33952a0a1982554b9eb6ef743caf6724c1ae0784
atcoder8 <atcoder8@gmail.com>
2023-12-13 02:53:10 +0900

    Solved ARC151.

--- arc151/src/bin/c.rs
+++ arc151/src/bin/c.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    
+    println!("{}", if solve() { "Takahashi" } else { "Aoki" });
+}
+
+fn solve() -> bool {
+    input! {
+        (n, m): (usize, usize),
+        xy: [(usize, usize); m],
+    }
+
+    if m == 0 {
+        return n % 2 != 0;
+    }
+
+    let mut grundy = xy[0].0 - 1 ^ n - xy[m - 1].0;
+    for window in xy.windows(2) {
+        let (x1, y1) = window[0];
+        let (x2, y2) = window[1];
+
+        if x1 + 1 != x2 && y1 == y2 {
+            grundy ^= 1;
+        }
+    }
+
+    grundy != 0
 }
