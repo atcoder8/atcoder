0af6a48864ae1c8114dfb2bea7dedbbf5eaf3e49
atcoder8 <atcoder8@gmail.com>
2023-12-29 17:47:28 +0900

    Solved ABC288-E.

--- abc288/src/bin/e.rs
+++ abc288/src/bin/e.rs
@@ -1,3 +1,4 @@
+use itertools::{enumerate, izip, Itertools};
 use proconio::{input, marker::Usize1};
 
 fn main() {
@@ -8,36 +9,62 @@ fn main() {
         xx: [Usize1; m],
     }
 
-    let mut req = vec![false; n];
+    let mut desired_flags = vec![false; n];
     for &x in &xx {
-        req[x] = true;
+        desired_flags[x] = true;
     }
 
-    let mut dp: Vec<Option<usize>> = vec![None; n + 1];
-    dp[0] = Some(0);
+    let calc_suffix_sum_cost = |item_idx: usize| {
+        let mut suffix_sum_cost = vec![cc[item_idx]; item_idx + 1];
+        for bought_num in 1..=item_idx {
+            suffix_sum_cost[bought_num] =
+                suffix_sum_cost[bought_num - 1].min(cc[item_idx - bought_num]);
+        }
 
-    for item_idx in 0..n {
-        let mut next_dp: Vec<Option<usize>> = if req[item_idx] {
-            vec![None; n + 1]
-        } else {
-            dp.clone()
-        };
+        suffix_sum_cost
+    };
 
-        for buy_cnt in 0..=item_idx {
-            if let Some(min_acc_cost) = dp[buy_cnt] {
-                let next_acc_cost = min_acc_cost + aa[item_idx] + cc[item_idx - buy_cnt];
+    let cost_mat = (0..n)
+        .map(|item_idx| calc_suffix_sum_cost(item_idx))
+        .collect_vec();
 
-                if next_dp[buy_cnt + 1].is_none() || next_acc_cost < next_dp[buy_cnt + 1].unwrap() {
-                    next_dp[buy_cnt + 1] = Some(next_acc_cost);
-                }
-            }
+    let mut dp = vec![Some(0)];
+    for (item_idx, (&a, &desired)) in enumerate(izip!(&aa, &desired_flags)) {
+        let mut next_dp = vec![None; item_idx + 2];
+        if !desired {
+            next_dp[..=item_idx].copy_from_slice(&dp);
+        }
+
+        for prev_bought_num in 0..=item_idx {
+            let Some(prev_cost) = dp[prev_bought_num] else { continue; };
+
+            let cand_cost = prev_cost + a + cost_mat[item_idx][prev_bought_num];
+            update_cost(&mut next_dp[prev_bought_num + 1], cand_cost);
         }
 
         dp = next_dp;
-
-        println!("dp = {:?}", dp);
     }
 
     let ans = dp.iter().filter_map(|&x| x).min().unwrap();
     println!("{}", ans);
+}
+
+/// Updates the minimum cost.
+/// If `cost` is `None`, always updated to the candidate cost.
+///
+/// # Arguments
+///
+/// * `cost` - Reference variable for the cost to be updated.
+/// * `cand_cost` - Candidate cost to update.
+pub fn update_cost<T>(cost: &mut Option<T>, cand_cost: T) -> bool
+where
+    T: PartialOrd,
+{
+    if cost.as_ref().is_some_and(|cost| cost <= &cand_cost) {
+        return false;
+    }
+
+    *cost = Some(cand_cost);
+
+    true
 }
