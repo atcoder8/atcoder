67d003a528b37b5b75181a3257cee3d461feb27e
atcoder8 <atcoder8@gmail.com>
2024-07-21 04:21:41 +0900

    Implemented `next_permutation` function and solved ABC363-C again.

--- abc363/src/bin/c.rs
+++ abc363/src/bin/c.rs
@@ -1,10 +1,9 @@
-use itertools::Itertools;
 use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         (n, k): (usize, usize),
-        s: Chars,
+        mut s: Chars,
     }
 
     let is_ok = |t: &[char]| {
@@ -17,11 +16,45 @@ fn main() {
         true
     };
 
-    let ans = s
-        .into_iter()
-        .permutations(n)
-        .unique()
-        .filter(|t| is_ok(t))
-        .count();
+    s.sort_unstable();
+
+    let mut ans = 0;
+    loop {
+        ans += is_ok(&s) as usize;
+
+        if !next_permutation(&mut s) {
+            break;
+        }
+    }
     println!("{}", ans);
+}
+
+/// If there is a next permutation of `seq` with respect to the lexicographic order, replace `seq` with it (return value is `true`).
+/// Otherwise (i.e., if `seq` is already in descending order), it reverts to ascending order (return value is `false`).
+pub fn next_permutation<T>(seq: &mut [T]) -> bool
+where
+    T: Ord,
+{
+    // If `seq` is empty, `false` is returned because there is no next permutation.
+    if seq.is_empty() {
+        return false;
+    }
+
+    // Find the maximum value of `i` such that `seq[i] < seq[i + 1]`.
+    // If no such `i` exists, `seq` has already been sorted in descending order,
+    // so return `false` after sorting in ascending order.
+    let Some(i) = (0..seq.len() - 1).rev().find(|&i| seq[i] < seq[i + 1]) else {
+        seq.reverse();
+        return false;
+    };
+
+    // Find the largest `j` that satisfies `j > i` and `seq[j] > seq[i]`, and exchange `seq[i]` and `seq[j]`.
+    // Because `seq[i] < seq[i + 1]`, there is always such `j`.
+    let j = (i + 1..seq.len()).rev().find(|&j| seq[j] > seq[i]).unwrap();
+    seq.swap(i, j);
+
+    // Sort elements after the `i`-th in ascending order to minimize the increase with respect to lexicographic order.
+    seq[i + 1..].reverse();
+
+    true
 }
