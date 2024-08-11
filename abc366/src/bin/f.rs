701230dd731911b6a74e0ec2b4e692ee91591793
atcoder8 <atcoder8@gmail.com>
2024-08-11 14:59:43 +0900

    Solved ABC366-F.

--- abc366/src/bin/f.rs
+++ abc366/src/bin/f.rs
@@ -2,6 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-        
+        (n, k): (usize, usize),
+        mut ab: [(usize, usize); n],
     }
+
+    ab.sort_unstable_by(|(a, b), (c, d)| ((a - 1) * d).cmp(&((c - 1) * b)));
+
+    let mut dp = vec![0_usize; k + 1];
+    dp[0] = 1;
+    for &(a, b) in &ab {
+        for i in (0..k).rev() {
+            dp[i + 1] = dp[i + 1].max(dp[i] * a + b);
+        }
+    }
+
+    println!("{}", dp[k]);
 }
