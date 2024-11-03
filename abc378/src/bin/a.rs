a1a32f3603ac5754c60e5f55cdf18f2191f4d563
pencoder <atcoder8@gmail.com>
2024-11-04 00:53:21 +0900

    Solved ABC378-A again.

--- abc378/src/bin/a.rs
+++ abc378/src/bin/a.rs
@@ -1,19 +1,16 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        mut aa: [usize; 4],
+        aa: [usize; 4],
     }
 
-    aa.sort_unstable();
-
-    let mut ans = 0_usize;
-    let mut hold = None;
-    for &a in &aa {
-        match hold.take() {
-            Some(prev) if prev == a => ans += 1,
-            _ => hold = Some(a),
-        }
-    }
+    let ans = aa
+        .iter()
+        .sorted_unstable()
+        .dedup_with_count()
+        .map(|(cnt, _)| cnt / 2)
+        .sum::<usize>();
     println!("{}", ans);
 }
