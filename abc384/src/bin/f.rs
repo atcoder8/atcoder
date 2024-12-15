3fc3d28f50926788b93149a0d4d3d98f7a3fe4b1
pencoder <atcoder8@gmail.com>
2024-12-15 15:59:50 +0900

    Solved ABC384-F.

--- abc384/src/bin/f.rs
+++ abc384/src/bin/f.rs
@@ -1,11 +1,8 @@
-// unfinished
-
-use std::ops::Add;
+use std::ops::{Add, AddAssign, Sub};
 
 use proconio::input;
 
-const MAX_EXP: u32 = 25;
-const RAISED: usize = 2_usize.pow(MAX_EXP);
+const MAX_EXP: u32 = 24;
 
 fn main() {
     input! {
@@ -13,25 +10,36 @@ fn main() {
         aa: [usize; n],
     }
 
-    let mut tree = vec![Node::default(); 2_usize.pow(MAX_EXP + 1)];
+    // 各expについて2^expを法として合同である要素をグループ化する
+    let mut init_nodes = vec![Node::default(); 2_usize.pow(MAX_EXP)];
     for &a in &aa {
-        tree[RAISED + a] = Node::new(1, a);
+        init_nodes[a] += Node::new(a);
     }
-    for i in (1..RAISED).rev() {
-        tree[i] = tree[2 * i] + tree[2 * i + 1];
+    let mut node_tree: Vec<Vec<Node>> = vec![vec![]; MAX_EXP as usize + 1];
+    node_tree[MAX_EXP as usize] = init_nodes;
+    for exp in (0..MAX_EXP).rev() {
+        let from_nodes = &node_tree[exp as usize + 1];
+        let mut nodes = vec![Node::default(); 2_usize.pow(exp)];
+        for i in 0..2_usize.pow(exp) {
+            nodes[i] = from_nodes[i] + from_nodes[i + 2_usize.pow(exp)];
+        }
+        node_tree[exp as usize] = nodes;
     }
 
     let mut ans = 0_usize;
     for &a in &aa {
-        // 末尾に0がexp個続く
-        let trailing = !a + 1;
+        let complementary = a.wrapping_neg();
         for exp in 0..MAX_EXP {
-            let mask = 2_usize.pow(exp) - 1;
-            let complementary = trailing & mask ^ 1;
-            let Node { count, sum } = tree[complementary];
+            let raised = 2_usize.pow(exp);
+            let Node { count, sum } =
+                node_tree[exp as usize + 1][complementary % (2 * raised) ^ raised];
             ans += (a * count + sum) >> exp;
         }
+        let Node { count, sum } = node_tree[MAX_EXP as usize][complementary % 2_usize.pow(MAX_EXP)];
+        ans += (a * count + sum) >> MAX_EXP;
     }
+    ans += aa.iter().map(|&a| a >> a.trailing_zeros()).sum::<usize>();
+    ans /= 2;
     println!("{}", ans);
 }
 
@@ -41,10 +49,10 @@ struct Node {
     sum: usize,
 }
 
-impl Add<Node> for Node {
+impl Add<Self> for Node {
     type Output = Node;
 
-    fn add(self, rhs: Node) -> Self::Output {
+    fn add(self, rhs: Self) -> Self::Output {
         Self::Output {
             count: self.count + rhs.count,
             sum: self.sum + rhs.sum,
@@ -52,8 +60,26 @@ impl Add<Node> for Node {
     }
 }
 
+impl Sub<Self> for Node {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            count: self.count - rhs.count,
+            sum: self.sum - rhs.sum,
+        }
+    }
+}
+
+impl AddAssign<Self> for Node {
+    fn add_assign(&mut self, rhs: Self) {
+        self.count += rhs.count;
+        self.sum += rhs.sum;
+    }
+}
+
 impl Node {
-    fn new(count: usize, sum: usize) -> Self {
-        Self { count, sum }
+    fn new(a: usize) -> Self {
+        Self { count: 1, sum: a }
     }
 }
