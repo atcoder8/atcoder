808cbe42727c1cdaa3d7e174fb3835d67ffec6ff
pencoder <atcoder8@gmail.com>
2025-03-23 18:16:18 +0900

    Solved ABC398-A again.

--- abc398/src/bin/a.rs
+++ abc398/src/bin/a.rs
@@ -5,11 +5,7 @@ fn main() {
         n: usize,
     }
 
-    if n % 2 == 0 {
-        let minus = "-".repeat((n - 2) / 2);
-        println!("{0}=={0}", minus);
-    } else {
-        let minus = "-".repeat((n - 1) / 2);
-        println!("{0}={0}", minus);
-    }
+    let num_equals = 2 - n % 2;
+    let num_minuses = (n - num_equals) / 2;
+    println!("{0}{1}{0}", "-".repeat(num_minuses), "=".repeat(num_equals));
 }
