3f5410b712199de84f85eef598d00601dd95908b
pencoder <atcoder8@gmail.com>
2025-04-06 13:55:27 +0900

    Participated in ABC400-C.

--- abc400/src/bin/c.rs
+++ abc400/src/bin/c.rs
@@ -6,9 +6,5 @@ fn main() {
         n: usize,
     }
 
-    let ans = (1..=n.sqrt())
-        .step_by(2)
-        .map(|b: usize| (0_usize..).find(|&i| (n / b.pow(2)) >> i == 1).unwrap())
-        .sum::<usize>();
-    println!("{}", ans);
+    println!("{}", (n / 2).sqrt() + (n / 4).sqrt());
 }
