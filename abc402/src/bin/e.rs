555afdb8865a6f72c05c33ad3f131541634aa3b1
pencoder <atcoder8@gmail.com>
2025-04-20 18:53:02 +0900

    Solved ABC402 again.

--- abc402/src/bin/e.rs
+++ abc402/src/bin/e.rs
@@ -1,4 +1,4 @@
-use itertools::{enumerate, Itertools};
+use itertools::{iproduct, Itertools};
 use ndarray::prelude::*;
 use proconio::input;
 
@@ -7,98 +7,50 @@ fn main() {
         (n, x): (usize, usize),
     }
 
-    let problems = (0..n).map(|_| Problem::read()).collect_vec();
+    let problems = (0..n).map(|i| Problem::read(i)).collect_vec();
 
-    let ans = recursion(
-        &problems,
-        x,
-        0,
-        &mut Array2::from_elem((x + 1, 1 << n), None),
-    );
-    println!("{}", ans);
+    let mut dp = Array2::from_elem((x + 1, 1 << n), 0.0_f64);
+    for (rem, bits) in iproduct!(0..=x, 0..1 << n) {
+        let calc_exp = |problem: &Problem| {
+            if problem.cost > rem || bits >> problem.id & 1 == 1 {
+                return 0.0;
+            }
+
+            let next_rem = rem - problem.cost;
+            let collect_exp = dp[(next_rem, bits | (1 << problem.id))] + problem.score;
+            let wrong_exp = dp[(next_rem, bits)];
+            collect_exp * problem.probability + wrong_exp * (1.0 - problem.probability)
+        };
+
+        dp[(rem, bits)] = problems
+            .iter()
+            .map(calc_exp)
+            .max_by(|x, y| x.partial_cmp(y).unwrap())
+            .unwrap();
+    }
+
+    println!("{}", dp[(x, 0)]);
 }
 
 #[derive(Debug, Clone, Copy)]
 struct Problem {
-    score: usize,
+    id: usize,
+    score: f64,
     cost: usize,
     probability: f64,
 }
 
 impl Problem {
-    fn read() -> Self {
+    fn read(id: usize) -> Self {
         input! {
-            (s, c, p): (usize, usize, f64),
+            (s, c, p): (f64, usize, f64),
         }
 
         Self {
+            id,
             score: s,
             cost: c,
             probability: p / 100.0,
         }
     }
-}
-
-fn recursion(
-    problems: &[Problem],
-    rem_money: usize,
-    bits: usize,
-    memo: &mut Array2<Option<f64>>,
-) -> f64 {
-    if let Some(exp) = memo[(rem_money, bits)] {
-        return exp;
-    }
-
-    let max_exp = enumerate(problems)
-        .filter_map(|(i, problem)| {
-            if bits >> i & 1 == 1 || problem.cost > rem_money {
-                return None;
-            }
-
-            let exp = (recursion(problems, rem_money - problem.cost, bits | 1 << i, memo)
-                + problem.score as f64)
-                * problem.probability
-                + recursion(problems, rem_money - problem.cost, bits, memo)
-                    * (1.0 - problem.probability);
-            Some(exp)
-        })
-        .max_by(|x, y| x.partial_cmp(y).unwrap())
-        .unwrap_or(0.0);
-
-    memo[(rem_money, bits)] = Some(max_exp);
-
-    max_exp
-}
-
-/// If the right-hand side is greater than the left-hand side,
-/// clones the right-hand side, bind it to the left-hand side,
-/// and return `true`.
-/// If the right-hand side is less than or equal to the left-hand side,
-/// does nothing and returns `false`.
-///
-/// The left-hand and right-hand sides must be the same type
-/// and must implement the `Clone` trait.
-///
-/// # Examples
-///
-/// ```
-/// let mut x = 5;
-///
-/// assert_eq!(chmax!(x, 3), false);
-/// assert_eq!(x, 5);
-///
-/// assert_eq!(chmax!(x, 7), true);
-/// assert_eq!(x, 7);
-/// ```
-#[macro_export]
-macro_rules! chmax {
-    ($lhs: expr, $rhs: expr) => {
-        if $rhs > $lhs {
-            let temp = $rhs.clone();
-            $lhs = temp;
-            true
-        } else {
-            false
-        }
-    };
 }
