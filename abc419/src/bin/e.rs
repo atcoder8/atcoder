7044140ccc5da7b3125f26acb8ab9eb3c6932c22
atcoder8 <atcoder8@gmail.com>
2025-08-17 19:38:22 +0900

    Solved ABC419 again.

--- abc419/src/bin/e.rs
+++ abc419/src/bin/e.rs
@@ -1,4 +1,5 @@
 use itertools::{enumerate, Itertools};
+use ndarray::Array2;
 use proconio::input;
 
 fn main() {
@@ -15,12 +16,14 @@ fn main() {
             .sum::<usize>()
     };
 
-    let init_costs = (0..m).map(|dest| calc_cost(0, dest)).collect_vec();
+    let cost_array = Array2::from_shape_fn((l, m), |(offset, dest)| calc_cost(offset, dest));
+
+    let init_costs = (0..m).map(|dest| cost_array[(0, dest)]).collect_vec();
     let costs_by_rem = (1..l).fold(init_costs, |costs, offset| {
         (0..m)
             .map(|to| {
                 enumerate(&costs)
-                    .map(|(from, &cost)| cost + calc_cost(offset, (to + m - from) % m))
+                    .map(|(from, &cost)| cost + cost_array[(offset, (to + m - from) % m)])
                     .min()
                     .unwrap()
             })
