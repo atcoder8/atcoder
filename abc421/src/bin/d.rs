9fe54a118a9a03dd64b2cb4e820ee3b2b8ba2c87
pencoder <atcoder8@gmail.com>
2025-08-31 12:40:50 +0900

    Solved ABC421 again.

--- abc421/src/bin/d.rs
+++ abc421/src/bin/d.rs
@@ -1,129 +1,62 @@
-use std::collections::VecDeque;
-
 use proconio::input;
 
 fn main() {
     input! {
-        (rt, ct, ra, ca): (i64, i64, i64, i64),
-        (_n, m, l): (usize, usize, usize),
-        sa: [(char, i64); m],
-        tb: [(char, i64); l],
+        (mut coord1, mut coord2): ((i64, i64), (i64, i64)),
+        (_n, m, l): (i64, usize, usize),
+        mut moves1: [(char, i64); m],
+        mut moves2: [(char, i64); l],
     }
 
-    let mut coord1 = [rt, ct];
-    let mut coord2 = [ra, ca];
-    let mut queue1: VecDeque<StraightMove> =
-        sa.iter().map(|&(s, a)| StraightMove::new(s, a)).collect();
-    let mut queue2: VecDeque<StraightMove> =
-        tb.iter().map(|&(t, b)| StraightMove::new(t, b)).collect();
+    moves1.reverse();
+    moves2.reverse();
 
-    let mut cross_times = 0_i64;
-    while let (Some(move1), Some(move2)) = (queue1.pop_front(), queue2.pop_front()) {
-        let StraightMove {
-            direction: dir1,
-            distance: dist1,
-        } = move1;
-        let StraightMove {
-            direction: dir2,
-            distance: dist2,
-        } = move2;
+    let mut num_crosses = 0_i64;
+    while let (Some(move1), Some(move2)) = (moves1.pop(), moves2.pop()) {
+        let (dir1, num_moves_1) = move1;
+        let (dir2, num_moves_2) = move2;
 
-        let min_dist = dist1.min(dist2);
+        let min_num_moves = num_moves_1.min(num_moves_2);
 
-        cross_times += calc_cross_times(coord1, coord2, dir1, dir2, min_dist);
-        coord1 = moved(coord1, dir1, min_dist);
-        coord2 = moved(coord2, dir2, min_dist);
-        if dist1 > min_dist {
-            queue1.push_front(StraightMove {
-                direction: dir1,
-                distance: dist1 - min_dist,
-            });
+        let rem_num_moves_1 = num_moves_1 - min_num_moves;
+        if rem_num_moves_1 > 0 {
+            moves1.push((dir1, rem_num_moves_1));
         }
-        if dist2 > min_dist {
-            queue2.push_front(StraightMove {
-                direction: dir2,
-                distance: dist2 - min_dist,
-            });
+        let rem_num_moves_2 = num_moves_2 - min_num_moves;
+        if rem_num_moves_2 > 0 {
+            moves2.push((dir2, rem_num_moves_2));
         }
+
+        let dist = calc_manhattan_dist(coord1, coord2);
+        if coord1 == coord2 {
+            num_crosses += if dir1 == dir2 { min_num_moves } else { 0 };
+        } else if dist % 2 == 0 && dist / 2 <= min_num_moves {
+            let mid_coord_1 = calc_moved_coord(coord1, dir1, dist / 2);
+            let mid_coord_2 = calc_moved_coord(coord2, dir2, dist / 2);
+            num_crosses += (mid_coord_1 == mid_coord_2) as i64;
+        }
+
+        coord1 = calc_moved_coord(coord1, dir1, min_num_moves);
+        coord2 = calc_moved_coord(coord2, dir2, min_num_moves);
     }
-    println!("{}", cross_times);
+
+    println!("{}", num_crosses);
 }
 
-fn dir_to_diffs(dir: char) -> [i64; 2] {
-    match dir {
-        'U' => [-1, 0],
-        'D' => [1, 0],
-        'L' => [0, -1],
-        'R' => [0, 1],
-        _ => panic!(),
-    }
+fn calc_manhattan_dist(coord1: (i64, i64), coord2: (i64, i64)) -> i64 {
+    let (r1, c1) = coord1;
+    let (r2, c2) = coord2;
+    (r1.abs_diff(r2) + c1.abs_diff(c2)) as i64
 }
 
-fn calc_cross_times(coord1: [i64; 2], coord2: [i64; 2], dir1: char, dir2: char, dist: i64) -> i64 {
-    if dir1 == dir2 {
-        return if coord1 == coord2 { dist } else { 0 };
-    }
-
-    let [x1, y1] = coord1;
-    let [x2, y2] = coord2;
-    let [dx, dy] = [x2 - x1, y2 - y1];
-
-    let [sx1, sy1] = dir_to_diffs(dir1);
-    let [sx2, sy2] = dir_to_diffs(dir2);
-    let [sx, sy] = [sx1 - sx2, sy1 - sy2];
-
-    if sx == 0 {
-        if dx != 0 || dy % sy != 0 {
-            return 0;
-        }
-
-        let t = dy / sy;
-        return (t > 0 && t <= dist) as i64;
-    }
-
-    if sy == 0 {
-        if dy != 0 || dx % sx != 0 {
-            return 0;
-        }
-
-        let t = dx / sx;
-        return (t > 0 && t <= dist) as i64;
-    }
-
-    if dx % sx != 0 || dy % sy != 0 {
-        return 0;
-    }
-
-    let tx = dx / sx;
-    let ty = dy / sy;
-
-    (tx > 0 && tx <= dist && tx == ty) as i64
-}
-
-fn moved(coord: [i64; 2], dir: char, dist: i64) -> [i64; 2] {
+fn calc_moved_coord(coord: (i64, i64), dir: char, num_moves: i64) -> (i64, i64) {
     let mut moved_coord = coord;
     match dir {
-        'U' => moved_coord[0] -= dist,
-        'D' => moved_coord[0] += dist,
-        'L' => moved_coord[1] -= dist,
-        'R' => moved_coord[1] += dist,
+        'U' => moved_coord.0 -= num_moves,
+        'D' => moved_coord.0 += num_moves,
+        'L' => moved_coord.1 -= num_moves,
+        'R' => moved_coord.1 += num_moves,
         _ => panic!(),
     }
-
     moved_coord
-}
-
-#[derive(Debug, Clone, Copy)]
-struct StraightMove {
-    direction: char,
-    distance: i64,
-}
-
-impl StraightMove {
-    fn new(direction: char, distance: i64) -> Self {
-        StraightMove {
-            direction,
-            distance,
-        }
-    }
 }
