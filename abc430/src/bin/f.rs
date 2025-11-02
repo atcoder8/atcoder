ee0b58e8f0e6a60b50fb55bbbbb283ac909574b6
pencoder <atcoder8@gmail.com>
2025-11-02 16:23:47 +0900

    Solved ABC430-F.

--- abc430/src/bin/f.rs
+++ abc430/src/bin/f.rs
@@ -1,6 +1,4 @@
-// unfinished
-
-use itertools::{enumerate, izip, Itertools};
+use itertools::{izip, Itertools};
 use proconio::{input, marker::Chars};
 
 fn main() {
@@ -12,6 +10,25 @@ fn main() {
     println!("{output}");
 }
 
+#[derive(Debug, Clone, Copy)]
+struct DirectionCounter {
+    direction: char,
+    count: usize,
+}
+
+impl DirectionCounter {
+    fn push_direction(&mut self, direction: char) {
+        if direction == self.direction {
+            self.count += 1;
+        } else {
+            *self = Self {
+                direction,
+                count: 1,
+            }
+        }
+    }
+}
+
 fn solve() -> Vec<i64> {
     input! {
         n: usize,
@@ -21,38 +38,42 @@ fn solve() -> Vec<i64> {
     let mut offset_left = vec![0_usize; n];
     let mut offset_right = vec![0_usize; n];
 
-    let mut prev = ('.', 0_usize);
-    for (i, &ch) in enumerate(&s) {
-        offset_left[i] = offset_left[i].max(prev.1 * (prev.0 == 'R') as usize);
-        offset_right[i] = offset_right[i].max(prev.1 * (prev.0 == 'L') as usize);
-
-        if ch == prev.0 {
-            prev.1 += 1;
+    let mut counter = DirectionCounter {
+        direction: 'L',
+        count: 0,
+    };
+    for i in 0..n {
+        if counter.direction == 'R' {
+            offset_left[i] += counter.count;
         } else {
-            prev = (ch, 1);
+            offset_right[i] += counter.count;
+        }
+
+        if i < n - 1 {
+            counter.push_direction(s[i]);
         }
     }
-    offset_left[n - 1] = offset_left[n - 1].max(prev.1 * (prev.0 == 'R') as usize);
-    offset_right[n - 1] = offset_right[n - 1].max(prev.1 * (prev.0 == 'L') as usize);
 
-    let mut prev = ('.', 0_usize);
-    for (i, &ch) in enumerate(&s).rev() {
-        offset_left[i + 1] = offset_left[i + 1].max(prev.1 * (prev.0 == 'L') as usize);
-        offset_right[i + 1] = offset_right[i + 1].max(prev.1 * (prev.0 == 'R') as usize);
-
-        if ch == prev.0 {
-            prev.1 += 1;
+    let mut counter = DirectionCounter {
+        direction: 'L',
+        count: 0,
+    };
+    for i in (0..n).rev() {
+        if counter.direction == 'L' {
+            offset_left[i] += counter.count;
         } else {
-            prev = (ch, 1);
+            offset_right[i] += counter.count;
+        }
+
+        if i > 0 {
+            counter.push_direction(s[i - 1]);
         }
     }
-    offset_left[0] = offset_left[0].max(prev.1 * (prev.0 == 'L') as usize);
-    offset_right[0] = offset_right[0].max(prev.1 * (prev.0 == 'R') as usize);
 
     let mut imos = vec![0_i64; n + 1];
-    for (&left, &right) in izip!(&offset_left, &offset_right) {
-        imos[left] += 1;
-        imos[n - right] -= 1;
+    for (&offset_left, &offset_right) in izip!(&offset_left, &offset_right) {
+        imos[offset_left] += 1;
+        imos[n - offset_right] -= 1;
     }
     for i in 0..n {
         imos[i + 1] += imos[i];
