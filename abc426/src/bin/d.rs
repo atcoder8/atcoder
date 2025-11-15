8acd479385e4957bdfb01ad24d2c35a8b1c6faa5
pencoder <atcoder8@gmail.com>
2025-11-15 13:29:50 +0900

    Solved ABC426-D again.

--- abc426/src/bin/d.rs
+++ abc426/src/bin/d.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use proconio::{input, marker::Chars};
+use proconio::input;
 
 fn main() {
     input! {
@@ -13,41 +13,17 @@ fn main() {
 fn solve() -> usize {
     input! {
         n: usize,
-        s: Chars,
+        s: String,
     }
 
-    let calc_acc_costs = |s: &[char], dest: char| {
-        let mut costs = vec![0_usize; n + 1];
-        let mut last_error_pos = 0_usize;
-        for i in 1..=n {
-            let ch = s[i - 1];
-            if ch == dest {
-                costs[i] = costs[i - 1];
-            } else {
-                costs[i] = costs[i - 1] + 2 * (i - 1 - last_error_pos) + 1;
-                last_error_pos = i;
-            }
-        }
-
-        costs
-    };
-
-    let find_min_cost = |dest: char| {
-        let mut s = s.clone();
-
-        let forward_costs = calc_acc_costs(&s, dest);
-        s.reverse();
-        let backward_costs = calc_acc_costs(&s, dest);
-
-        (0..=n)
-            .map(|i| forward_costs[i] + backward_costs[n - i])
-            .min()
-            .unwrap()
-    };
-
-    ['0', '1']
-        .iter()
-        .map(|&dest| find_min_cost(dest))
+    let num_zeros = s.chars().filter(|&ch| ch == '0').count();
+    let counts = [num_zeros, n - num_zeros];
+    s.chars()
+        .dedup_with_count()
+        .map(|(cnt, dest)| {
+            let num_dest_chars = counts[dest.to_digit(10).unwrap() as usize];
+            2 * (num_dest_chars - cnt) + n - num_dest_chars
+        })
         .min()
         .unwrap()
 }
